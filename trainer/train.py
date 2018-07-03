0cb66884e66b2213f3fa53e1f22e1887eb3f9bb3
Anderson <a141890@gmail.com>
2018-07-03 17:58:59 +0900

    Change steps & epochs

--- trainer/train.py
+++ trainer/train.py
@@ -19,8 +19,8 @@ def parse_args(args):
     parser = argparse.ArgumentParser(description='Retinanet training script')
     parser.add_argument('data_mode')
     parser.add_argument('data_path', default='/data/VOCdevkit/')
-    parser.add_argument('--steps', type=int, default=1, help='number of steps per epoch')
-    parser.add_argument('--epochs', type=int, default=200, help='number of epochs')
+    parser.add_argument('--steps', type=int, default=10000, help='number of steps per epoch')
+    parser.add_argument('--epochs', type=int, default=500, help='number of epochs')
 
     # Backbone
     parser.add_argument('--backbone', default='resnet101', type=str, help='Backbone model (resnet50)')
